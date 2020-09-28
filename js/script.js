f3765acefbe77dbd52efafe30401419b57e514e3
Eddie <eddiedelfert@gmail.com>
2020-09-28 15:48:07 -0700

    added tags, random colors, and a timer

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ const quotes = [
   {quote: 'Trying to define yourself is like trying to bite your own teeth.', source: 'Alan Watts'},
   {quote: 'What good is the warmth of summer, without the cold of winter to give it sweetness.', source: 'John Steinbeck', citation: 'Travels with Charley'},
   {quote: 'The man who does not read good books has no advantage over the man who cannot read them.', source: 'Mark Twain', year: '1895'},
-  {quote: 'Sometimes even to live is an act of courage.', source: 'Seneca'},
-  {quote: 'Freedom is nothing but a chance to be better.', source: 'Albert Camus'},
+  {quote: 'Sometimes even to live is an act of courage.', source: 'Seneca', tag: 'Stoicism'},
+  {quote: 'Freedom is nothing but a chance to be better.', source: 'Albert Camus', tag: 'Existentialism'},
 
 ]
 
@@ -43,19 +43,30 @@ function printQuote () {
   //Create another variable to store HTML String
   let html = `<p class="quote">${randomPrint.quote} </p> <p class="source"> ${randomPrint.source} `
   
-  //Check if quote has a year or citation
+  //Check if quote has a year, citation, or tag
   if (randomPrint.year){
     html = `${html} <span class="year"> ${randomPrint.year} </span> </p>` 
   } 
   if (randomPrint.citation){
      html = `${html} <span class="citation"> ${randomPrint.citation} </span> </p>`
   } 
+  if (randomPrint.tag){
+    html = `${html} <span class="citation"> ${randomPrint.tag} </span> </p>`
+  }
 
   //Print to html
   document.getElementById('quote-box').innerHTML = html
+
+  //Change background color when printQuote is run
+  //Equation found on Stack Overflow here : https://rb.gy/awutls
+  document.body.style.backgroundColor =  '#' + (0x1000000+(Math.random()) * 0xffffff).toString(16).substr(1,6)
 }
 
-
+// setInterval function
+// Help setting up found at https://rb.gy/hzzmy6
+const interval = setInterval (function() {
+  printQuote()
+}, 5000);
 
 /***
  * click event listener for the print quote button
